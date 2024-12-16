6f23f9ff61c60ff8478adfc136bdc31fc58a109e
Ofry <ofryhlely06@gmail.com>
2024-12-16 15:54:36 +0200

    hehe

--- client.py
+++ client.py
@@ -3,57 +3,68 @@ import sys
 import os
 
 BUFFER_SIZE = 1024
+OK = 200
+REDIRECT = 301
+NOT_FOUND = 404
 
 def main():
-	server_ip = sys.argv[1] 
-	server_port = int(sys.argv[2])
+	serverIp = sys.argv[1] 
+	serverPort = int(sys.argv[2])
+	# Creating socket
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect((server_ip,server_port))
+	try_again = False
+	closed_connection = True
 
 	while True:
-		filePath = input()
+		# If it's in redirect we don't want to get input
+		if (not try_again):
+			filePath = input()
+		try_again = False
 		fileName = get_file_name(filePath)
-		# Construct msg
-		request = (
-			f"GET {filePath} HTTP/1.1\r\n"
-			f"Connection: keep-alive\r\n"
-			f"\r\n"
-		)
-		# Convert to bytes
-		s.send(request.encode('utf-8'))
-		# Getting data from the server
+		# If the connection is closed we restart it
+		if (closed_connection):
+			s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			s.connect((serverIp,serverPort))
+			closed_connection = False
+		request = construct_msg(filePath, try_again)
+		s.send(request.encode())
+		# Getting reply from the server
 		serverReply = get_all_data(s)
-
-		# Getting the first line
-		firstLine = get_first_line(serverReply)
-		print(firstLine)
-
-		statusCode = firstLine.split(" ")[1]
-		if statusCode == "200":
-			handle_200(serverReply,fileName)
-		if statusCode == "404":
+		if (not serverReply):
+			closed_connection = True
+			try_again = True
+			s.close()
 			continue
-		if statusCode == "301":
-			# TO DO: EXTRACT LOCATION FROM SERVER
-			# SEND NEW REQUEST
+		# Getting the first line
+		status, firstLine, fileContent, linesDict = parse_response(serverReply)
+		print(firstLine)
+		if(status == OK):
+			create_the_file(fileContent, fileName)
+			if (connection_closed(linesDict)):
+				closed_connection = True
+				s.close()
+			continue
+		elif(status == REDIRECT):
+			filePath = get_location(linesDict)
+			try_again = True
+		closed_connection = True
+		s.close()
+
 
 def get_file_name(filePath):
-	return os.path.basename(filePath)
+	return "index.html" if filePath == "/" else os.path.basename(filePath)
 
 def get_all_data(con):
-    """
-        input: 
-            con: socket
-        output:
-            all the data in a variable
-    """
-    data = b""
-    while True:
-        newData = con.recv(BUFFER_SIZE)
-        if not newData: 
-            break
-        data += newData
-    return data
+	data = b""
+	while True:
+		# Adding up the data
+		newData = con.recv(BUFFER_SIZE)
+		if not newData: 
+			break
+		data += newData
+	if (data == b""):
+		return None
+	return data
 
 def get_content(data):
 	delimiter = b"\r\n\r\n"
@@ -70,22 +81,58 @@ def create_the_file(fileContent, fileName):
 	if fileName.endswith(('.ico', '.jpg')):
 		with open(file_path, "wb") as file:
 			file.write(fileContent)
-	else:	
-	    # Writing the content to the file
-	    with open(file_path, "w") as file:
-	        file.write(fileContent.decode('utf-8'))
+		file.close()
+	else:
+		fileContent = fileContent.decode()	
+		# Writing the content to the file
+		with open(file_path, "w") as file:
+			file.write(fileContent)
+		file.close()
+	
+
 
 def get_first_line(serverReply):
-    lines = serverReply.splitlines()
-    if isinstance(serverReply, bytes):
-        return lines[0].decode('utf-8')
-    else:
-        return lines[0]
+	response = serverReply
+	lines = response.splitlines()
+	return lines[0]
 
-def handle_200(serverReply, fileName):
-	fileContent = get_content(serverReply)
-	# Create file and fill it
-	create_the_file(fileContent,fileName)
+
+def get_location(lines):
+    return lines.get("Location", "")
+
+def construct_msg(path, redirectFlag):
+	if (not redirectFlag):
+		return 	(f"GET {path} HTTP/1.1\r\n"
+				f"Connection: keep-alive\r\n"
+				f"\r\n")
+	return 	(f"GET {path} HTTP/1.1\r\n"
+			f"Connection: close\r\n"
+			f"\r\n")
+
+
+def parse_response(serverReply):
+	firstLine = get_first_line(serverReply)
+	statusCode = int(firstLine.decode().split(" ")[1])
+	if (statusCode == OK):
+		file = get_content(serverReply)
+	else:
+		file = None	
+	# separating the reply into lines
+	delimiter = b"\r\n\r\n"
+	index = serverReply.find(delimiter)
+	headers_section = serverReply[:index].decode()
+	lines = headers_section.splitlines()
+	headers = lines[1:]
+	headers_dict = {}
+	for header in headers:
+		if header:
+			key, value = header.split(":", 1)
+			headers_dict[key.strip()] = value.strip()
+
+	return statusCode, firstLine, file, headers_dict
+
+def connection_closed(linesDict):
+	return linesDict.get("connection", "").lower() == "close"
 
 
 if __name__ == "__main__":
